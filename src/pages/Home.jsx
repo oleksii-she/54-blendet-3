cd93a7c7748652e02f1371bbb314798aa1b9f3c9
Oleksii Shevchenko <vazerius@gmail.com>
2022-11-12 15:10:30 +0200

    +

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,21 @@
 import { Container, CountryList, Heading, Loader, Section } from 'components';
-
+import { useEffect, useState } from 'react';
+import { getCountries } from 'service/country-service';
 export const Home = () => {
+  const [country, setCountry] = useState([]);
+
+  useEffect(() => {
+    const CoutiestApi = async () => {
+      try {
+        const reponce = await getCountries();
+        if (reponce.length > 0) {
+          setCountry(reponce);
+        }
+      } catch (error) {}
+    };
+    CoutiestApi();
+  }, []);
+
   return (
     <Section>
       <Container>
